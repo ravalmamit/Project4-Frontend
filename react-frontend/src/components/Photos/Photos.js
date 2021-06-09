e5db11bee47a7e08ae31d74ee8fd1efd4ffb3010
amitmraval <amitmraval@yahoo.com>
2021-06-08 21:18:30 -0400

    photos slider slick changes

--- react-frontend/src/components/Photos/Photos.js
+++ react-frontend/src/components/Photos/Photos.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import condo1 from "../../images/condo1.jpeg";
 import condo2 from "../../images/condo2.jpeg";
 import condo3 from "../../images/condo3.jpeg";
@@ -7,7 +9,7 @@ import condo4 from "../../images/condo4.jpeg";
 import condo5 from "../../images/condo5.jpeg";
 import condo6 from "../../images/condo6.jpeg";
 import condo7 from "../../images/condo7.jpeg";
-
+import Header from "../Header/Header";
 import "./Photos.css";
 
 const images = [condo1, condo2, condo3, condo4, condo5, condo6, condo7];
@@ -16,20 +18,24 @@ function Photos(props) {
   const settings = {
     infinite: true,
     lazyLoad: true,
+    arrows: true,
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 1,
     centerMode: true,
     centerPadding: 0,
   };
   return (
     <div>
-      <Slider className="images">
-        {images.map((img, idx) => (
-          <div>
-            <img src={img} alt={img} />
-          </div>
-        ))}
-      </Slider>
+      <Header />
+      <div className="photos">
+        <Slider {...settings} className="images">
+          {images.map((img, idx) => (
+            <div>
+              <img src={img} alt={img} />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 }
