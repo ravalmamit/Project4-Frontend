991c2bbe6882486a28dfa24aeafe05bfc2f98311
amitmraval <amitmraval@yahoo.com>
2021-06-09 19:18:37 -0400

    photo updates

--- react-frontend/src/components/Photos/Photos.js
+++ react-frontend/src/components/Photos/Photos.js
@@ -2,23 +2,15 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import condo1 from "../../images/condo1.jpeg";
-import condo2 from "../../images/condo2.jpeg";
-import condo3 from "../../images/condo3.jpeg";
-import condo4 from "../../images/condo4.jpeg";
-import condo5 from "../../images/condo5.jpeg";
-import condo6 from "../../images/condo6.jpeg";
-import condo7 from "../../images/condo7.jpeg";
-import Header from "../Header/Header";
+import SliderData from "./SliderData";
 import "./Photos.css";
-
-const images = [condo1, condo2, condo3, condo4, condo5, condo6, condo7];
+import Header from "../Header/Header";
 
 function Photos(props) {
   const settings = {
     infinite: true,
-    lazyLoad: true,
     arrows: true,
+    lazyLoad: true,
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
@@ -27,11 +19,13 @@ function Photos(props) {
   return (
     <div>
       <Header />
+      {console.log(SliderData)}
       <div className="photos">
         <Slider {...settings} className="images">
-          {images.map((img, idx) => (
+          {console.log(SliderData)}
+          {SliderData.map((image, idx) => (
             <div>
-              <img src={img} alt={img} />
+              <img key={idx} src={image} alt={idx} />
             </div>
           ))}
         </Slider>
---
+++ react-frontend/src/components/Photos/SliderData.js
@@ -0,0 +1,33 @@
+import condo1 from "../../images/condo1.jpeg";
+import condo2 from "../../images/condo2.jpeg";
+import condo3 from "../../images/condo3.jpeg";
+import condo4 from "../../images/condo4.jpeg";
+import condo5 from "../../images/condo5.jpeg";
+import condo6 from "../../images/condo6.jpeg";
+import condo7 from "../../images/condo7.jpeg";
+import condo8 from "../../images/condo8.jpeg";
+import condo9 from "../../images/condo9.jpeg";
+import condo10 from "../../images/condo10.jpeg";
+import condo11 from "../../images/condo11.jpeg";
+import condo12 from "../../images/condo12.jpeg";
+import condo13 from "../../images/condo13.jpeg";
+import condo14 from "../../images/condo14.jpeg";
+
+const SliderData = [
+  condo1,
+  condo2,
+  condo3,
+  condo4,
+  condo5,
+  condo6,
+  condo7,
+  condo8,
+  condo9,
+  condo10,
+  condo11,
+  condo12,
+  condo13,
+  condo14,
+];
+
+export default SliderData;
